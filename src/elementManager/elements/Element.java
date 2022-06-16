6d486d6228599bf5875096573d6dbcde870004e5
joseph <95222288+josephosan@users.noreply.github.com>
2022-06-16 19:20:29 +0430

    solving

--- src/elementManager/elements/Element.java
+++ src/elementManager/elements/Element.java
@@ -1,26 +1,58 @@
 package elementManager.elements;
 
+import config.Config;
 import java.awt.*;
 import javax.swing.*;
+import elementManager.coordinate.*;;
 import elementManager.coordinate.*;
+import elementManager.*;
 
 public class Element {
+    String id;
     JPanel panel;
     Position position;
+    AaPosition position;
+    AaSize size;
+    ElementManager elementManager = Config.getElementManager();
     public Element(JPanel panel) {
         this.panel = panel;
         this.position = new Position();
+        this.position = new AaPosition();
+        this.size = new AaSize();
     }
 
     public Position getPosition() {
-        return this.position;
-    }
+        public AaPosition getPosition() {
+            return this.position;
+        }
 
-    public JPanel getPanel() {
-        return this.panel;
-    }
+        public JPanel getPanel() {
+            return this.panel;
+        }
 
-    public void paintSelf(Graphics g) {
+        public AaSize getSize() {
+            return this.size;
+        }
 
+        public void setId(String id) {
+            this.id = id;
+        }
+
+        public String getId() {
+            return this.id;
+        }
+
+        // joins group
+        public void join(String group) {
+            elementManager.joinGroup(group, id);
+        }
+
+        // leaves group
+        public void leave(String group) {
+            elementManager.leaveGroup(group, id);
+        }
+
+        public void paintSelf(Graphics g) {
+
+        }
     }
-}
