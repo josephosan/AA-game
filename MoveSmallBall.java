8a1b89dac25ad3cb3b7874b6c680954e2edb44f3
danial azimi <93203936+danilazm@users.noreply.github.com>
2022-06-18 17:27:46 +0430

    make movesmallball middleware

---
+++ MoveSmallBall.java
@@ -0,0 +1,26 @@
+package middlewareManager.middlewares;
+
+import elementManager.elements.SmallBall;
+
+public class MoveSmallBall extends Middleware  {
+    SmallBall smallBall;
+
+    int endY;
+    
+    public MoveSmallBall(SmallBall smallBall){
+    super("moveSmallBall");
+    this.smallBall = smallBall;
+    }
+
+    @Override
+    public void run() {
+        this.smallBall.getPosition().setY(this.smallBall.getPosition().getY()-10);;
+        if(this.smallBall.getPosition().getY()==endY){
+            this.remove();
+            return;
+        }
+    
+    }
+
+}
+
