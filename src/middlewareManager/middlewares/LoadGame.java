897812f1a6ef9707ecf8c1509ac0d855342353f0
mahbod <m.ghadirisani2013@gmail.com>
2022-06-20 16:51:20 +0430

    "Load Game" Done

--- src/middlewareManager/middlewares/LoadGame.java
+++ src/middlewareManager/middlewares/LoadGame.java
@@ -19,10 +19,8 @@ public class LoadGame extends Middleware {
 
     //THIS CLASS DOES NOT CURRENTLY SUPPORT OBJECT ARGUEMENT FOR CONSTRUCTORS!
     //THIS CLASS STILL NEED A PARSER!
-    //DO NOT MERGE WITH MASTER YET!
     @Override
     public void init(){
-        System.out.println("LOAD GAME INIT");
         HashMap<String,HashMap<String,String>> data = new HashMap<String, HashMap<String,String>>();
         //Loading from file
         File file = new File("./../../data/levels/"+level+".aa");
@@ -32,16 +30,8 @@ public class LoadGame extends Middleware {
         }
         else{
             System.out.println("Level file not found!");
+            return;
         }
-
-        HashMap<String,String> h = new HashMap<String,String>();
-
-        //THIS PART IS FOR TEST AND SHOULD BE REMOVED LATER
-        // h.put("className","middlewareManager.middlewares.ShowMenu"); //name
-        // h.put("numberOfArgs","0"); //number of args
-        // h.put("arg0","");
-        //data.put("middleware0",h);
-        //END OF TEST SECTION
         
         int i = 0;
         while(true){
@@ -111,7 +101,6 @@ public class LoadGame extends Middleware {
                 }
                 catch(Exception e){
                     e.printStackTrace();
-                    System.out.println("AN ERROR HAPPENED");
                     break;
                 }
                 
@@ -124,22 +113,5 @@ public class LoadGame extends Middleware {
         this.remove();
         
     }
-
-    private boolean isNumeric(String string){
-        try{
-            Integer.parseInt(string);
-            return true;
-        }
-        catch(NumberFormatException e){
-            return false;
-        }
-
-    }
-
-    private boolean isMiddleware(String string){
-        if(Character.isUpperCase(string.charAt(0))) return true;
-        return false;
-    }
-
 
 }
