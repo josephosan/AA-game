623e2369de24b79bb192203f077cfaffc7aa8574
mahbod <m.ghadirisani2013@gmail.com>
2022-06-21 19:09:19 +0430

    Fixed "DrawSmallBall" and "SmallBall"

--- src/utils/calculations/Angle.java
+++ src/utils/calculations/Angle.java
@@ -3,18 +3,21 @@ package utils.calculations;
 public class Angle {
     private double angle;
 
-    Angle(Double angle){
+    public Angle(int angle){
+        this.angle = (double)angle;
+    }
+    public Angle(Double angle){
         this.angle = angle;
     }
-    Angle(){
+    public Angle(){
         this.angle = 90;    //The constructor default to 90 degree as it is the starting position.
     }
 
-    double getInRadian(){
+    public double getInRadian(){
         return Math.toRadians(this.angle);
     }
 
-    void add(double degree){
+    public void add(double degree){
         this.angle += degree;
     }
 
