3429b8503734f5bee00b97e90b75611b6f1ffd84
mahbod <m.ghadirisani2013@gmail.com>
2022-06-22 11:30:47 +0430

    Further changes to SmallBall behaviour.

--- src/middlewareManager/middlewares/DrawSmallBall.java
+++ src/middlewareManager/middlewares/DrawSmallBall.java
@@ -1,35 +1,40 @@
 package middlewareManager.middlewares;
 
-import java.util.ArrayList;
 import config.Config;
-import utils.calculations.Rotation;
 import elementManager.ElementManager;
-import elementManager.elements.*;
+import elementManager.elements.SmallBall;
+import frameManager.FrameManager;
+import utils.calculations.Angle;;
 
-public class DrawSmallBall extends Middleware {
+//This middleware create a SmallBall instance.
+//Then it will add it to "rotatingSmallBalls" Group.
+//The constructor accepts String so it can work around "LoadGame" shortcomings.
+//The us of "n" variable is to ensure that we won't use duplicate id.
+
+public class DrawSmallBall extends Middleware{
+    static int n = 0;   //to keep track how many times this middleware has been called.
+    final int id;       // to store n before n changes as a result of this middleware being called again. 
+    Angle angle;
     ElementManager elementManager = Config.getElementManager();
-    BigBall bigBall;
-    ArrayList<Element> rotatingSmallBalls;
-    Rotation rotator;
+    FrameManager frameManager = Config.getFrameManager();
 
-    public DrawSmallBall(BigBall bigBall){
-        super("drawSmallBall");
-        this.bigBall = bigBall;
+    DrawSmallBall(String angle){
+        super("AddSmallBall"+n);
+        id = n;
+        n++;
+        this.angle = new Angle(Double.parseDouble(angle));
     }
 
     @Override
     public void init(){
-        rotator = new Rotation(3, bigBall); //TODO rotationSpeed should be passed as a var.
+        //creatin SmallBall instance
+        SmallBall smallBall = new SmallBall(frameManager.getAPanel("game"),angle);
+        smallBall.setNumber(id);
+        //adding smallBall to elementManager
+        elementManager.addElement("smallBall"+id, smallBall);
+        //adding SmallBall to "rotatingSmallBalls" Group.
+        elementManager.joinGroup("rotatingSmallBalls", "smallBall"+id);
+        this.remove();
     }
-    @Override
-    public void run(){
-        rotatingSmallBalls = elementManager.getElementsByGroup("rotatingSmallBalls");
-        
-        for(Element smallBall : rotatingSmallBalls){
-            rotator.rotate((SmallBall)smallBall);
-        }
-
-        //TODO if game is over remove self
-    }
-
+    
 }
--- src/utils/calculations/Rotation.java
+++ src/utils/calculations/Rotation.java
@@ -2,18 +2,34 @@ package utils.calculations;
 
 import java.lang.Math;
 
+import config.Config;
+import elementManager.ElementManager;
 import elementManager.coordinate.AaPosition;
 import elementManager.elements.*;
 
 public class Rotation{
-    private final Integer rotationSpeed; //should be in degree
+    private Integer rotationSpeed; //should be in degree
     private final AaPosition origin; //coordinates of center of the Big Ball
     private final Integer r; // radius of the Main Circle
 
-    public Rotation(Integer rotationSpeed , BigBall bigBall){
-        this.rotationSpeed = rotationSpeed;
+    public Rotation(){
+        this.rotationSpeed = 2;
+        ElementManager elementManager = Config.getElementManager();
+        BigBall bigBall =  (BigBall)elementManager.getElementById("bigBall");
         this.origin = bigBall.getPosition();
         this.r = bigBall.getR();
+    }
+
+    public Rotation(Integer rotationSpeed){
+        this.rotationSpeed = rotationSpeed;
+        ElementManager elementManager = Config.getElementManager();
+        BigBall bigBall =  (BigBall)elementManager.getElementById("bigBall");
+        this.origin = bigBall.getPosition();
+        this.r = bigBall.getR();
+    }
+
+    public void setSpeed(Integer rotationSpeed){
+        this.rotationSpeed = rotationSpeed;
     }
 
     public void rotate(SmallBall smallBall){
