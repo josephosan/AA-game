2930c60b5fc1f454236240a6fcaa9c50aa021981
danial <danmy7799@gmail.com>
2022-06-22 15:50:59 +0430

    finish MoveSmallBall

--- src/middlewareManager/middlewares/MoveSmallBall.java
+++ src/middlewareManager/middlewares/MoveSmallBall.java
@@ -1,15 +1,21 @@
 package middlewareManager.middlewares;
-
+import config.Config;
+import elementManager.ElementManager;
+import elementManager.elements.BigBall;
 import elementManager.elements.SmallBall;
 
 public class MoveSmallBall extends Middleware  {
     SmallBall smallBall;
-
+    BigBall bigBall;
     int endY;
     
     public MoveSmallBall(SmallBall smallBall){
     super("moveSmallBall");
     this.smallBall = smallBall;
+    ElementManager elementManager= Config.getElementManager();
+    bigBall = (BigBall)elementManager.getElementById("bigBall");
+    endY = bigBall.getR()+bigBall.getPosition().getY();
+
     }
 
     @Override
