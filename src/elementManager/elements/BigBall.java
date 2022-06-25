3853f63343e7077f35d6b745092d583c71b9a825
joseph <95222288+josephosan@users.noreply.github.com>
2022-06-25 18:49:43 +0430

    borna Maker added

--- src/elementManager/elements/BigBall.java
+++ src/elementManager/elements/BigBall.java
@@ -3,16 +3,36 @@ package elementManager.elements;
 import config.Config;
 import elementManager.coordinate.AaPosition;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.awt.image.ImagingOpException;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Objects;
+
 public class BigBall extends Element{
     public AaPosition aaPosition = new AaPosition();
     Color color;
+    int bornaXPos = 0;
+    int bornaYPos = 0;
+    private Image bornaImage;
+
     public BigBall(JPanel panel) {
         super(panel);
         this.size.setSize(Config.getMainCircleSize().getWidth(), Config.getMainCircleSize().getHeight());
         aaPosition.setX(Config.getFrameWidth()/2);
         aaPosition.setY(this.size.getHeight() + Config.getBigCircleRadios() + 50);
+
+        // borna stuff:
+        try {
+            bornaImage = ImageIO.read(new File("src/Icons/borna.jpg"));
+        } catch (ImagingOpException | IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public AaPosition getPosition() {
@@ -28,6 +48,11 @@ public class BigBall extends Element{
         this.color = c;
     }
 
+    public void setBornaPosition(int xPos, int yPos) {
+        this.bornaXPos = xPos;
+        this.bornaYPos = yPos;
+    }
+
 
     @Override
     public void paintSelf(Graphics g) {
@@ -38,6 +63,8 @@ public class BigBall extends Element{
             this.position.getY(),
             this.size.getWidth(), 
             this.size.getHeight());
+        if (bornaXPos != 0 || bornaYPos != 0)
+            g2D.drawImage(bornaImage, bornaXPos, bornaYPos, 60, 60, null);
     }
 
     public Integer getR() {
