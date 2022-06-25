28274d5eb3558ca1071f9e56c4cbe012e7f20a3a
joseph <95222288+josephosan@users.noreply.github.com>
2022-06-25 19:03:15 +0430

    some small chenges on BigBall

--- src/elementManager/elements/BigBall.java
+++ src/elementManager/elements/BigBall.java
@@ -6,19 +6,18 @@ import elementManager.coordinate.AaPosition;
 import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
-import java.awt.image.BufferedImage;
 import java.awt.image.ImagingOpException;
 import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.IOException;
-import java.util.Objects;
+
 
 public class BigBall extends Element{
     public AaPosition aaPosition = new AaPosition();
     Color color;
     int bornaXPos = 0;
     int bornaYPos = 0;
+    int bornaXSize = 60;
+    int bornaYSize = 60;
     private Image bornaImage;
 
     public BigBall(JPanel panel) {
@@ -53,6 +52,11 @@ public class BigBall extends Element{
         this.bornaYPos = yPos;
     }
 
+    public void setBornaSize(int xSize, int ySize) {
+        this.bornaXSize = xSize;
+        this.bornaYSize = ySize;
+    }
+
 
     @Override
     public void paintSelf(Graphics g) {
@@ -64,7 +68,7 @@ public class BigBall extends Element{
             this.size.getWidth(), 
             this.size.getHeight());
         if (bornaXPos != 0 || bornaYPos != 0)
-            g2D.drawImage(bornaImage, bornaXPos, bornaYPos, 60, 60, null);
+            g2D.drawImage(bornaImage, bornaXPos, bornaYPos, bornaXSize, bornaYSize, null);
     }
 
     public Integer getR() {
